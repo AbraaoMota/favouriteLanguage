48d3162f814449df8e7cbd5ecd07fc58a515f52b
Abraao Mota <abraaomot@outlook.com>
2016-12-20 20:53:22 +0000

    WIP: Initial work on user favourite language

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,2 +1,9 @@
 class GithubUser < ActiveRecord::Base
+
+  def calculate_favourite_language(repos)
+    languages = repos.map { |repo| repo.language }
+    language_hash_count = languages.inject(Hash.new(0)) { |lang, count| lang.update(count => lang[count] + 1 ) }
+    most_used = language_hash_count.keep_if { |lang, count| count == language_hash_count.values.max }
+    languages = most_used.keys
+  end
 end
